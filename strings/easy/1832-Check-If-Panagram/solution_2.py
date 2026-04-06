b6b34f035b52e9632a9c5549badfab468fbacb03
sruthimadira7 <sruthimadira51@gmail.com>
2026-04-06 12:09:46 +0530

    problem: 1832-pangram-checker

---
+++ strings/easy/1832-Check-If-Panagram/solution_2.py
@@ -0,0 +1,51 @@
+"""
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+Example 1:
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains every letter of the English alphabet at least once.
+
+Example 2:
+Input: sentence = "leetcode"
+Output: false
+
+Constraints:
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
+"""
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        #  check the length of the string less than 26
+        #  if it is, it is not an Pangram
+        if len(sentence) < 26:
+            return False
+        # take an array containing 26 values 
+        #  which is an equivalent representation of 26 letters
+        temp = [0] * 26
+        # store the unicode value of charcater 'a'
+        ord_a = ord('a')
+
+        # itearte the given sentence
+        for ch in sentence:
+            #  calculate the equivalent index represent of english letter in alphabets
+            idx = ord(ch) - ord_a
+            #  increment the character count by 1 each time
+            temp[idx] += 1
+        
+        # iterate the temp
+        for val in temp:
+            # if any of the equivalemt characters value is equal to 'zero'
+            if val == 0:
+                #  it's not a Panagram
+                return False
+            
+        # If it contains all the letters atleast once
+        #  It's a Panagram
+        return True
+
+
+s = Solution()
+print(s.checkIfPangram("thequickbrownfoxjumpsoverthelazydog"))
+print(s.checkIfPangram("leetcode"))
