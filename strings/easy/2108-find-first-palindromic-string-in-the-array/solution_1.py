4350d54ada20ae9f5a6258ae296e0a559b19308d
sruthimadira7 <sruthimadira51@gmail.com>
2026-04-25 22:31:22 +0530

    problem: 2108-find-first-palindromic-string-in-the-array

---
+++ strings/easy/2108-find-first-palindromic-string-in-the-array/solution_1.py
@@ -0,0 +1,54 @@
+"""
+2108. Find First Palindromic String in the Array
+Given an array of strings words, return the first palindromic string in the array.
+If there is no such string, return an empty string "".
+A string is palindromic if it reads the same forward and backward.
+
+Example 1:
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.
+
+Example 2:
+Input: words = ["notapalindrome","racecar"]
+Output: "racecar"
+Explanation: The first and only string that is palindromic is "racecar".
+
+Example 3:
+Input: words = ["def","ghi"]
+Output: ""
+Explanation: There are no palindromic strings, so the empty string is returned.
+
+Constraints:
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists only of lowercase English letters.
+"""
+class Solution:
+    def firstPalindrome(self, words: list[str]) -> str:
+        def check_palindrome(s : str) -> bool:
+            left = 0
+            right = len(s) - 1
+
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                
+                left += 1
+                right -= 1
+
+            return True
+    
+        for word in words:
+            if check_palindrome(word):
+                return word
+            
+        return ''
+            
+            
+
+
+        
+s = Solution()
+print(s.firstPalindrome(["notapalindrome","racecar"]))
