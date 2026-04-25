e670b103bbae7cba0c6bea84403b3fd04ce83ff1
sruthimadira7 <sruthimadira51@gmail.com>
2026-04-25 18:47:33 +0530

    update solution

--- strings/easy/680-valid-palindrome-II/solution_1.py
+++ strings/easy/680-valid-palindrome-II/solution_1.py
@@ -20,16 +20,32 @@ s consists of lowercase English letters.
 """
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
+        def check_palindrome(s, i, j):
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                
+                i += 1
+                j -= 1
+            
+            return True
 
-        
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] != s[i]:
+                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
+            
+            i += 1
+            j -= 1
 
-        
 
+        return True
 
 s = Solution()
 print(s.validPalindrome("cbbcc")) # True
 print(s.validPalindrome("abc")) # False
 print(s.validPalindrome("zryxeededexyz")) # False
-print(s.validPalindrome("eceec")) # True
+# print(s.validPalindrome("eceec")) # True
+print(s.validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga")) # True
+# a g u o k e p a t g b n v f q m g m l c 
