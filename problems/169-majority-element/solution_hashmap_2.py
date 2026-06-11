b054a2dc5ac2d831cb97a1402c1c6651cb7c91b1
sruthimadira7 <sruthimadira51@gmail.com>
2026-06-11 13:24:43 +0530

    add solution_hashmap_2 to 169-majority-element

---
+++ problems/169-majority-element/solution_hashmap_2.py
@@ -0,0 +1,32 @@
+"""
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+The input is generated such that a majority element will exist in the array.
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+"""
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        majority_ele_counter = Counter(nums)
+
+        return max(majority_ele_counter.keys(), key=majority_ele_counter.get)
+
+            
+s = Solution()
+print(s.majorityElement([2,2,1,1,1,2,2]))
